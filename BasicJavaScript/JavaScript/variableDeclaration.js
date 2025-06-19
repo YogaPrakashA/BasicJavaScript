630b62bb309319238491d662b9d0869b9dc63a3e
YogaPrakashA <asprakash1208@gmail.com>
2025-06-19 15:45:13 +0530

    JavaScript Study materials

---
+++ BasicJavaScript/JavaScript/variableDeclaration.js
@@ -0,0 +1,15 @@
+console.info(x);// Undefined
+var x = 3;  // it's support old-browser and leading bugs because var by default value is undefined
+
+
+console.info(z); // ReferenceError: Cannot access 'z' before initialization (not initialization yet)
+let z= 0;  // Support modern programming
+
+
+console.info(y); // ReferenceError: Cannot access 'y' before initialization (not initialization yet)
+const y = 1;  // constant varible, we can't change value 
+
+
+
+
+
